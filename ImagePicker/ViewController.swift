dcfec757fbb2de4e26d7141e6765b1e585fc06a0
iamtonyarmstrong <i.am@iamtonyarmstrong.com>
2015-05-06 08:46:56 -0400

    First commit of code and README

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func imageButtonTapped(sender: UIButton) {
+
+        // Create the imagePicker and set the source type(s) for the services available
+        var imagePicker:UIImagePickerController = UIImagePickerController()
+        var sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+
+        if(UIImagePickerController.isSourceTypeAvailable(sourceType)){
+            imagePicker.sourceType = sourceType
+            imagePicker.delegate = self
+
+            // This is how you show a new ViewController
+            presentViewController(imagePicker, animated: true, completion: nil)
+        }
+
+    }
+
+    // MARK: - ImagePicker and NavigationController Delegates
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+
+    }
+
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,2 @@
+# ImagePicker
+Test code for how to generate an ImagePicker with Swift
