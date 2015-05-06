3f69225147d7a2fd9718b68c9a225e69f0bd39f7
iamtonyarmstrong <i.am@iamtonyarmstrong.com>
2015-05-05 21:29:57 -0400

    Initial Commit

---
+++ ImagePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by Anthony Armstrong on 5/5/15.
+//  Copyright (c) 2015 openinformant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
